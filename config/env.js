b038d0ed6c9861cc3eadcbc3331e9648e1239633
zhChameleon <mauroliu77@gmail.com>
2021-03-18 21:07:31 +0800

    flashloan bot

--- config/env.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    API_URL: 'http://localhost:3001',
-    APP_PATH: '/usr/src/app',
-    CONFIG_PATH: '/usr/src/app/config'
-}
--- src/data.js
+++
@@ -1,7 +0,0 @@
-//data.js
-const data = [
-    { id: 1, username: 'vikram', email:'vmelkote@ucsc.edu'},
-    { id: 2, username: 'mike', email:'mijhamil@ucsc.edu'}
-]
-
-module.exports = data;
